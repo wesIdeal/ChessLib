23cae0752e2e23fba5426a1121181d0b1cd93910
Wes Idell <wesidell@gmail.com>
2019-03-12 21:04:40 -0400

    Added benchmarks project

---
+++ MagicBitboard.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using MagicBitboard.Benchmarks.Helpers;
+using System;
+
+namespace MagicBitboard.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BitHelpersBenchmark>();
+        }
+    }
+}
