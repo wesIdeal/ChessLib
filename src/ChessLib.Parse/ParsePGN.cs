23446df9bfaba2a43de4117116c8cac2830834e7
Wes Idell <wesidell@gmail.com>
2019-03-20 13:10:04 -0400

    Split PGN Database into games before ANTLR processing.

--- src/ChessLib.Parse/ParsePGN.cs
+++ src/ChessLib.Parse/ParsePGN.cs
@@ -7,6 +7,7 @@ using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
 using ChessLib.Parse.Parser.Base;
 using ChessLib.Parse.Parser;
+using System;
 
 namespace ChessLib.Parse
 {
@@ -18,9 +19,15 @@ namespace ChessLib.Parse
     public class ParsePGN
     {
         AntlrFileStream fileStream;
+        AntlrInputStream inputStream;
+        public const string GameRegEx = @"(?<pgnGame>\s*(?:\[\s*(?<tagName>\w+)\s*""(?<tagValue>[^""]*)""\s*\]\s*)+(?:(?<moveNumber>\d+)(?<moveMarker>\.|\.{3})\s*(?<moveValue>(?:[PNBRQK]?[a-h]?[1-8]?x?[a-h][1-8](?:\=[PNBRQK])?|O(-?O){1,2})[\+#]?(\s*[\!\?]+)?)(?:\s*(?<moveValue2>(?:[PNBRQK]?[a-h]?[1-8]?x?[a-h][1-8](?:\=[PNBRQK])?|O(-?O){1,2})[\+#]?(\s*[\!\?]+)?))?\s*(?:\(\s*(?<variation>(?:(?<varMoveNumber>\d+)(?<varMoveMarker>\.|\.{3})\s*(?<varMoveValue>(?:[PNBRQK]?[a-h]?[1-8]?x?[a-h][1-8](?:\=[PNBRQK])?|O(-?O){1,2})[\+#]?(\s*[\!\?]+)?)(?:\s*(?<varMoveValue2>(?:[PNBRQK]?[a-h]?[1-8]?x?[a-h][1-8](?:\=[PNBRQK])?|O(-?O){1,2})[\+#]?(\s*[\!\?]+)?))?\s*(?:\((?<varVariation>.*)\)\s*)?(?:\{(?<varComment>[^\}]*?)\}\s*)?)*)\s*\)\s*)*(?:\{(?<comment>[^\}]*?)\}\s*)?)*(?<endMarker>1\-?0|0\-?1|1/2\-?1/2|\*)?\s*)";
         public ParsePGN(string pgnPath)
         {
-            fileStream = new AntlrFileStream(pgnPath);
+            var pgnText = System.IO.File.ReadAllText(pgnPath);
+            var splitPgn = Regex.Matches(pgnText, GameRegEx);
+           
+           
+            inputStream = new AntlrInputStream(pgnText);
         }
 
 
@@ -31,7 +38,7 @@ namespace ChessLib.Parse
         }
         private void TestWalkingListener()
         {
-            PGNLexer lexer = new PGNLexer(fileStream);
+            PGNLexer lexer = new PGNLexer(inputStream);
             var tokens = new CommonTokenStream(lexer);
             var parser = new PGNParser(tokens);
             var parseTree = parser.parse();
