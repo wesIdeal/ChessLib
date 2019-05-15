3d98a234c0b8f31b09d7bb352d663912d94cc469
Wes Idell <wesidell@gmail.com>
2019-05-15 15:24:26 -0400

    Moved tags

---
+++ src/ChessLib.Data/Tags.cs
@@ -0,0 +1,32 @@
+﻿using ChessLib.Data.Helpers;
+using System.Collections.Generic;
+
+namespace ChessLib.Data
+{
+    public class Tags : Dictionary<string, string>
+    {
+        private readonly string[] requiredTags = new string[] { "Event", "Site", "Date", "Round", "White", "Black", "Result" };
+        public Tags()
+        {
+            foreach (var requiredTag in requiredTags)
+            {
+                Add(requiredTag, requiredTag);
+            }
+        }
+
+        public bool HasSetup => this.ContainsKey("SetUp") && this["SetUp"] == "1";
+        public string FENStart => HasSetup ? this["FEN"] : FENHelpers.FENInitial;
+
+        public string White => ContainsKey("White") ? this["White"] : "";
+        public string Black => ContainsKey("Black") ? this["Black"] : "";
+
+        public new void Add(string key, string value)
+        {
+            if (ContainsKey(key))
+            {
+                this[key] = value;
+            }
+            else { base.Add(key, value); }
+        }
+    }
+}
