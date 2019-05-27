6bac29272254d1c76293e4e465f1550b8f74f2f3
Wes Idell <wesidell@gmail.com>
2019-05-26 21:50:02 -0400

    *fix* Added PiecePlacement

---
+++ src/ChessLib.Data/Boards/PiecePlacement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChessLib.Data.Helpers;
+using ChessLib.Types;
+using ChessLib.Types.Enums;
+using ChessLib.Types.Interfaces;
+
+namespace ChessLib.Data.Boards
+{
+    public class PiecePlacement
+    {
+        protected ulong[][] _pieces = new ulong[2][];
+        public ulong BlackOccupancy => ColorOccuancy(Color.Black);
+        public ulong WhiteOccupancy => ColorOccuancy(Color.Black);
+
+        public ulong PieceOfColorOccupancy(PieceOfColor poc) => PieceOfColorOccupancy(poc.Color, poc.Piece);
+
+        public ulong PieceOfColorOccupancy(Color color, Piece piece) => _pieces.Occupancy(color, piece);
+
+        public ulong[][] GetBoardPostMove(in IMoveExt move) => BoardHelpers.GetBoardPostMove(_pieces, move);
+
+        public void ApplyMove(in IMoveExt move) => _pieces = GetBoardPostMove(move);
+
+        private ulong ColorOccuancy(Color c) => _pieces.Occupancy(c);
+
+        public ulong BlackPiecesOfType(Piece p) =>
+             _pieces[(int)Color.Black][(int)p];
+        public ulong WhitePiecesOfType(Piece p) =>
+            _pieces[(int)Color.White][(int)p];
+
+    }
+}
