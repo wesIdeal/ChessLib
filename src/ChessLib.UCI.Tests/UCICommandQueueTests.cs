44ec88a5f5aede02a01e7ff8d3f5ba0061e353ce
Wes Idell <wesidell@gmail.com>
2019-07-07 22:17:23 -0400

    Test fixes

--- src/ChessLib.UCI.Tests/UCICommandQueueTests.cs
+++ src/ChessLib.UCI.Tests/UCICommandQueueTests.cs
@@ -45,13 +45,12 @@ namespace ChessLib.UCI.Tests
                 q.Enqueue(new CommandInfo(AppToUCICommand.IsReady));
             }
             Assert.AreEqual(expectedSize, q.Count());
-            q.Enqueue(new CommandInfo(interruptCommand));
+            q.Enqueue(new InterruptCommand(interruptCommand));
             Assert.AreEqual(1, q.Count());
         }
 
         [TestCase(AppToUCICommand.Quit, true)]
         [TestCase(AppToUCICommand.Stop, true)]
-        [TestCase(AppToUCICommand.UCI, false)]
         public void TestEnqueueInterruptCommand_SendsAppropriateEvent(AppToUCICommand interruptCommand, bool shouldReceiveInterruptEvent)
         {
             var expectedSize = 5;
@@ -62,7 +61,7 @@ namespace ChessLib.UCI.Tests
                 q.Enqueue(new CommandInfo(AppToUCICommand.IsReady));
             }
             Assert.AreEqual(expectedSize, q.Count());
-            q.Enqueue(new CommandInfo(interruptCommand));
+            q.Enqueue(new InterruptCommand(interruptCommand));
             var waitHandle = WaitHandle.WaitAny(new[] { q.InterruptIssued }, timeout);
             Assert.AreEqual(expectedWaitHandle, waitHandle);
         }
