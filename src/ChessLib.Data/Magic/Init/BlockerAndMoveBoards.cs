9f91648d069895a336f971193986306e8c94b77b
Wes Idell <wesidell@gmail.com>
2019-08-01 18:15:24 -0400

    Blocker and Move Board class put into Magic/Init

--- src/ChessLib.Data/Magic/Init/BlockerAndMoveBoards.cs
+++ src/ChessLib.Data/Magic/Init/BlockerAndMoveBoards.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using ChessLib.Data.Types.Interfaces;
 
-namespace ChessLib.Data
+namespace ChessLib.Data.Magic.Init
 {
     public class BlockerAndMoveBoards : IBlockerAndMoveBoards
     {
