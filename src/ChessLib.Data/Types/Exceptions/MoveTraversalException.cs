9d1b389b1b5f8fdfcaf89772c1c05c20ef479b40
Wes Idell <wesidell@gmail.com>
2019-08-08 14:04:53 -0400

    Traversal service started

---
+++ src/ChessLib.Data/Types/Exceptions/MoveTraversalException.cs
@@ -0,0 +1,28 @@
+﻿using ChessLib.Data.Types.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessLib.Data.Types.Exceptions
+{
+    public class MoveTraversalException : Exception
+    {
+        public static MoveTraversalException NextMoveNotFoundException(IMove move)
+        {
+            return new MoveTraversalException("Move {move} not found.");
+        }
+
+        public static MoveTraversalException NextMoveNotFoundException(IMoveStorage move)
+        {
+            if (string.IsNullOrWhiteSpace(move.SAN))
+            {
+                return new MoveTraversalException($"Move {move.SAN} not found. ");
+            }
+            return NextMoveNotFoundException(move.MoveData);
+        }
+
+        protected MoveTraversalException(string message) : base(message)
+        {
+        }
+    }
+}
